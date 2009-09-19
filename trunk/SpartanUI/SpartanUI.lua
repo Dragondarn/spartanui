948c8c101649f2a9e325ca4c9593f33521ffcbe0
beladona@wowcompares.com <beladona@wowcompares.com@b79ec1bc-9efc-11de-b798-49b661d4457b>
2009-09-19 00:03:40 +0000

    Now using AceAddon for addon creation and management. Each major section of the addon now exist as "modules" that can be enabled or disabled using Ace3 commands.

---
+++ trunk/SpartanUI/SpartanUI.lua
@@ -0,0 +1,34 @@
+local addon = LibStub("AceAddon-3.0"):NewAddon("SpartanUI","AceConsole-3.0");
+----------------------------------------------------------------------------------------------------
+suiChar = suiChar or {};
+addon.options = {name = "SpartanUI", type = "group", args = {}};
+
+function addon:OnInitialize()
+	addon.options.args["reset"] = {
+		type = "execute",
+		name = "Reset Options",
+		desc = "resets all options to default",
+		func = function()
+			if (InCombatLockdown()) then 
+				addon:Print(ERR_NOT_IN_COMBAT);
+			else
+				suiChar = nil;
+				ReloadUI();
+			end
+		end
+	};
+end
+function addon:OnEnable()
+	LibStub("AceConfig-3.0"):RegisterOptionsTable("SpartanUI", addon.options, {"sui", "spartanui"});
+end
+function addon:MergeData(target,source)
+	if type(target) ~= "table" then target = {} end
+	for k,v in pairs(source) do
+		if type(v) == "table" then
+			target[k] = self:MergeData(target[k], v);
+		else
+			target[k] = v;
+		end
+	end
+	return target;
+end
--- trunk/SpartanUI/global/ChatFrame.lua
+++ trunk/SpartanUI/global/ChatFrame.lua
@@ -1,76 +1,77 @@
-local NUM_SCROLL_LINES = 3; -- number of lines to jump when mouse-wheel scrolling
-local function noop() return; end
-local function hide(this) this:Hide(); end
-local function scroll(this, arg1)
-	if arg1 > 0 then
-		if IsShiftKeyDown() then
-			this:ScrollToTop()
-		elseif IsControlKeyDown() then
-			this:PageUp()
-		else
-			for i = 1, NUM_SCROLL_LINES do
-				this:ScrollUp()
-			end
-		end
-	elseif arg1 < 0 then
-		if IsShiftKeyDown() then
-			this:ScrollToBottom()
-		elseif IsControlKeyDown() then
-			this:PageDown()
-		else
-			for i = 1, NUM_SCROLL_LINES do
-				this:ScrollDown()
-			end
-		end
-	end
-end
-
-do -- addon detection and modifications to the chat frame
+local addon = LibStub("AceAddon-3.0"):GetAddon("SpartanUI");
+local module = addon:NewModule("ChatFrame");
+---------------------------------------------------------------------------
+function module:OnEnable()
 	if (Prat or ChatMOD_Loaded or ChatSync or Chatter or PhanxChatDB) then
-		-- do nothing since an addon is doing it all; I may add some things here based on testing though...
+		-- do nothing since an addon is doing it all; This is here for future expansion...
 	else
-		SpartanUI:HookScript("OnEvent",function()
+		local NUM_SCROLL_LINES = 3;
+		local noop = function() return; end;
+		local hide = function(this) this:Hide(); end;
+		local scroll = function(this, arg1)
+			if arg1 > 0 then
+				if IsShiftKeyDown() then
+					this:ScrollToTop()
+				elseif IsControlKeyDown() then
+					this:PageUp()
+				else
+					for i = 1, NUM_SCROLL_LINES do
+						this:ScrollUp()
+					end
+				end
+			elseif arg1 < 0 then
+				if IsShiftKeyDown() then
+					this:ScrollToBottom()
+				elseif IsControlKeyDown() then
+					this:PageDown()
+				else
+					for i = 1, NUM_SCROLL_LINES do
+						this:ScrollDown()
+					end
+				end
+			end
+		end;
+		do -- modify chat frames
 			for i = 1,7 do
 				local frame = _G["ChatFrame"..i];				
 				frame:SetMinResize(64,40); frame:SetFading(0);
 				frame:EnableMouseWheel(true);
 				frame:SetScript("OnMouseWheel",scroll);
-				
+			
 				frame:SetFrameStrata("MEDIUM");
 				frame:SetToplevel(false);
 				frame:SetFrameLevel(2);
-			
+		
 				local button = _G["ChatFrame"..i.."UpButton"]
 				button:SetScript("OnShow", hide)
-				button:Hide()
-			
-				button = _G["ChatFrame"..i.."DownButton"]
-				button:SetScript("OnShow", hide)
-				button:Hide()
-
-				button = _G["ChatFrame"..i.."BottomButton"]
-				button:ClearAllPoints()
-				button:SetPoint("BOTTOMLEFT", frame, "BOTTOMRIGHT", -32, -4)
-				button:Hide()
-			end
+				button:Hide();
 		
+				button = _G["ChatFrame"..i.."DownButton"];
+				button:SetScript("OnShow", hide);
+				button:Hide();
+				button = _G["ChatFrame"..i.."BottomButton"];
+				button:ClearAllPoints();
+				button:SetPoint("BOTTOMLEFT", frame, "BOTTOMRIGHT", -32, -4);
+				button:Hide();
+			end
+			
 			ChatFrameMenuButton:SetParent(DEFAULT_CHAT_FRAME);
 			ChatFrameMenuButton:ClearAllPoints(); ChatFrameMenuButton:SetScale(0.6);
-			ChatFrameMenuButton:SetPoint("TOPLEFT",DEFAULT_CHAT_FRAME,"TOPRIGHT",-29,4);		
+			ChatFrameMenuButton:SetPoint("TOPLEFT",DEFAULT_CHAT_FRAME,"TOPRIGHT",-29,4);
 			
 			hooksecurefunc("ChatFrame_OnUpdate",function(frame,elapsed)
 				local button = _G[frame:GetName().."BottomButton"];
 				if button and frame:AtBottom() then
 					button:Hide();
 				elseif button then
-					button:Show();
+				button:Show();
 				end
 			end);			
 			FCF_SetButtonSide = noop;		
-			
+		
 			ChatFrameEditBox:ClearAllPoints();
 			ChatFrameEditBox:SetPoint("BOTTOMLEFT",  ChatFrame1, "TOPLEFT", 0, 2);
 			ChatFrameEditBox:SetPoint("BOTTOMRIGHT", ChatFrame1, "TOPRIGHT", 0, 2);
-		end);
+		end
 	end
 end
