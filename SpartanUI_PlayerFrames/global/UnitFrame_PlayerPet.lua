9435e86e5d1302abeef30d0ac87b5977fdd56006
beladona <beladona@wowcompares.com>
2009-10-04 15:05:53 +0000

    issue 28 issue 29 issue 30

---
+++ SpartanUI_PlayerFrames/global/UnitFrame_PlayerPet.lua
@@ -0,0 +1,7 @@
+local spartan = LibStub("AceAddon-3.0"):GetAddon("SpartanUI");
+local addon = spartan:GetModule("PlayerFrames");
+----------------------------------------------------------------------------------------------------
+oUF:SetActiveStyle("Spartan_PlayerFrames");
+
+addon.pet = oUF:Spawn("focus","SUI_PetFrame");
+addon.pet:SetPoint("BOTTOMRIGHT",SpartanUI,"TOP",-370,12);
