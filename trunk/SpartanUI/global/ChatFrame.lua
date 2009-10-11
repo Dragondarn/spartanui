bb13f4e77ed4d4ce8a5496d67602e4fb3a04ffd3
beladona@wowcompares.com <beladona@wowcompares.com@b79ec1bc-9efc-11de-b798-49b661d4457b>
2009-10-11 16:59:20 +0000

    issue 8
    Optimized the code so that it acts on every frame chat frame rather than just ChatFrame1. Also added better logic for handling the hide/show states of the menu button and up, down, and bottom buttons. Some code credited to yo3fxy

--- trunk/SpartanUI/global/ChatFrame.lua
+++ trunk/SpartanUI/global/ChatFrame.lua
@@ -2,82 +2,81 @@ if (Prat or ChatMOD_Loaded or ChatSync or Chatter or PhanxChatDB) then return; e
 local addon = LibStub("AceAddon-3.0"):GetAddon("SpartanUI");
 local module = addon:NewModule("ChatFrame");
 ---------------------------------------------------------------------------
-function module:OnEnable()
-	local NUM_SCROLL_LINES = 3;
-	local noop = function() return; end;
-	local hide = function(this) this:Hide(); end;
-	local scroll = function(this, arg1)
-		if arg1 > 0 then
-			if IsShiftKeyDown() then
-				this:ScrollToTop()
-			elseif IsControlKeyDown() then
-				this:PageUp()
-			else
-				for i = 1, NUM_SCROLL_LINES do
-					this:ScrollUp()
-				end
-			end
-		elseif arg1 < 0 then
-			if IsShiftKeyDown() then
-				this:ScrollToBottom()
-			elseif IsControlKeyDown() then
-				this:PageDown()
-			else
-				for i = 1, NUM_SCROLL_LINES do
-					this:ScrollDown()
-				end
-			end
-		end
-	end;
-	do -- modify chat frames
-		for i = 1,7 do
-			local frame = _G["ChatFrame"..i];				
-			frame:SetMinResize(64,40); frame:SetFading(0);
-			frame:EnableMouseWheel(true);
-			frame:SetScript("OnMouseWheel",scroll);
-		
-			frame:SetFrameStrata("MEDIUM");
-			frame:SetToplevel(false);
-			frame:SetFrameLevel(2);
-	
-			local button = _G["ChatFrame"..i.."UpButton"];
-			button:ClearAllPoints(); button:SetScale(0.8);
-			button:SetPoint("BOTTOMRIGHT",DEFAULT_CHAT_FRAME,"RIGHT",4,0);
-			
-			button = _G["ChatFrame"..i.."DownButton"];
-			button:ClearAllPoints(); button:SetScale(0.8);
-			button:SetPoint("TOPRIGHT",DEFAULT_CHAT_FRAME,"RIGHT",4,0);
-	
-			button = _G["ChatFrame"..i.."BottomButton"];
-			button:ClearAllPoints(); button:SetScale(0.8);
-			button:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", 4, -10);
-		end
-		
-		ChatFrameMenuButton:SetParent(DEFAULT_CHAT_FRAME);
-		ChatFrameMenuButton:ClearAllPoints();
-		ChatFrameMenuButton:SetScale(0.8);
-		ChatFrameMenuButton:SetPoint("TOPLEFT",DEFAULT_CHAT_FRAME,"TOPRIGHT",-29,4);
-		
-		DEFAULT_CHAT_FRAME:HookScript("OnUpdate",function()
-			if MouseIsOver(DEFAULT_CHAT_FRAME) then
-				ChatFrame1UpButton:Show();
-				ChatFrame1DownButton:Show();
-				ChatFrameMenuButton:Show();
-			else
-				ChatFrame1UpButton:Hide();
-				ChatFrame1DownButton:Hide();
-				ChatFrameMenuButton:Hide();
-			end
-			if ChatFrame1:AtBottom() then
-				ChatFrame1BottomButton:Hide();
-			else
-				ChatFrame1BottomButton:Show();
-			end
-		end);
-		FCF_SetButtonSide = noop;		
-
-		ChatFrameEditBox:ClearAllPoints();
-		ChatFrameEditBox:SetPoint("BOTTOMLEFT",  ChatFrame1, "TOPLEFT", 0, 2);
-		ChatFrameEditBox:SetPoint("BOTTOMRIGHT", ChatFrame1, "TOPRIGHT", 0, 2);
+local ChatHoverFunc = function(frame)
+	if MouseIsOver(frame) then
+		frame.UpButton:Show();
+		frame.DownButton:Show();
+		if frame == DEFAULT_CHAT_FRAME then ChatFrameMenuButton:Show(); end
+	else
+		frame.UpButton:Hide();
+		frame.DownButton:Hide();
+		if frame == DEFAULT_CHAT_FRAME then ChatFrameMenuButton:Hide(); end
 	end
+	if frame:AtBottom() then
+		frame.BottomButton:Hide();
+	else
+		frame.BottomButton:Show();
+	end
+end;
+local noop = function() return; end;
+local hide = function(this) this:Hide(); end;
+local NUM_SCROLL_LINES = 3;	
+local scroll = function(this, arg1)
+	if arg1 > 0 then
+		if IsShiftKeyDown() then
+			this:ScrollToTop()
+		elseif IsControlKeyDown() then
+			this:PageUp()
+		else
+			for i = 1, NUM_SCROLL_LINES do
+				this:ScrollUp()
+			end
+		end
+	elseif arg1 < 0 then
+		if IsShiftKeyDown() then
+			this:ScrollToBottom()
+		elseif IsControlKeyDown() then
+			this:PageDown()
+		else
+			for i = 1, NUM_SCROLL_LINES do
+				this:ScrollDown()
+			end
+		end
+	end
+end;
+
+function module:OnEnable()	
+	for i = 1,7 do
+		local frame = _G["ChatFrame"..i];				
+		frame:SetMinResize(64,40); frame:SetFading(0);
+		frame:EnableMouseWheel(true);
+		frame:SetScript("OnMouseWheel",scroll);
+		frame:SetFrameStrata("MEDIUM");
+		frame:SetToplevel(false);
+		frame:SetFrameLevel(2);
+		
+		frame.UpButton = _G["ChatFrame"..i.."UpButton"];
+		frame.UpButton:ClearAllPoints(); frame.UpButton:SetScale(0.8);
+		frame.UpButton:SetPoint("BOTTOMRIGHT",frame,"RIGHT",4,0);
+		
+		frame.DownButton = _G["ChatFrame"..i.."DownButton"];
+		frame.DownButton:ClearAllPoints(); frame.DownButton:SetScale(0.8);
+		frame.DownButton:SetPoint("TOPRIGHT",frame,"RIGHT",4,0);
+			
+		frame.BottomButton = _G["ChatFrame"..i.."BottomButton"];
+		frame.BottomButton:ClearAllPoints(); frame.BottomButton:SetScale(0.8);
+		frame.BottomButton:SetPoint("BOTTOMRIGHT", frame, "BOTTOMRIGHT", 4, -10);
+		
+		frame:HookScript("OnUpdate",ChatHoverFunc);
+	end
+		
+	ChatFrameMenuButton:SetParent(DEFAULT_CHAT_FRAME);
+	ChatFrameMenuButton:ClearAllPoints();
+	ChatFrameMenuButton:SetScale(0.8);
+	ChatFrameMenuButton:SetPoint("TOPRIGHT",DEFAULT_CHAT_FRAME,"TOPRIGHT",4,4);
+	FCF_SetButtonSide = noop;
+	
+	ChatFrameEditBox:ClearAllPoints();
+	ChatFrameEditBox:SetPoint("BOTTOMLEFT",  ChatFrame1, "TOPLEFT", 0, 2);
+	ChatFrameEditBox:SetPoint("BOTTOMRIGHT", ChatFrame1, "TOPRIGHT", 0, 2);
 end
