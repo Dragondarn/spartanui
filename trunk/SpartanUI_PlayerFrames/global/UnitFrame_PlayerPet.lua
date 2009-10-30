2648fc579042fedd9173055eabe5e0f2b9104dc3
beladona@wowcompares.com <beladona@wowcompares.com@b79ec1bc-9efc-11de-b798-49b661d4457b>
2009-10-30 19:13:29 +0000

    Issue 47: Removed UNIT_CLASIFICATION_CHANGED event registration on the class icon, as this appears to be unnecessary. After testing I was able to force an update to the name and class icon using the UNIT_PET event. The fix for the pet name is included in the UnitFrame_PlayerPet.lua file.
    Also removed the dependency on oUF_ClassIcons and changed the icon name so as to prevent collision. Installing ClassIcons module will cause no conflict, nor will it change anything with the class icons.
    
    Removed Adapt dependancy, pending addition of 3d portrait code in 2.6.1

--- trunk/SpartanUI_PlayerFrames/global/UnitFrame_PlayerPet.lua
+++ trunk/SpartanUI_PlayerFrames/global/UnitFrame_PlayerPet.lua
@@ -5,3 +5,12 @@ oUF:SetActiveStyle("Spartan_PlayerFrames");
 
 addon.pet = oUF:Spawn("pet","SUI_PetFrame");
 addon.pet:SetPoint("BOTTOMRIGHT",SpartanUI,"TOP",-370,12);
+
+do -- fix for switching pets when a pet already exists
+	local updateName = function(self,event)
+		if self.Name then self.Name:UpdateTag(self.unit); end
+	end
+	addon.pet:RegisterEvent("UNIT_PET",updateName);
+end
+	
+	
