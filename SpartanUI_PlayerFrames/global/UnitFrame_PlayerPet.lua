d63d41f581edaf605821f33abaaebac00557de89
beladona <beladona@wowcompares.com>
2009-10-31 15:10:59 +0000

    fix for issue 48, pending testing by yo3fxy: modified the updateName function in UnitFrame_PlayerPet.lua so that it updates both the name and the level on the pet frame. Added UNIT_CLASSIFICATION_CHANGED as a commented out event registration so that it can be tested if UNIT_PET isn't enough.

--- SpartanUI_PlayerFrames/global/UnitFrame_PlayerPet.lua
+++ SpartanUI_PlayerFrames/global/UnitFrame_PlayerPet.lua
@@ -7,10 +7,12 @@ addon.pet = oUF:Spawn("pet","SUI_PetFrame");
 addon.pet:SetPoint("BOTTOMRIGHT",SpartanUI,"TOP",-370,12);
 
 do -- fix for switching pets when a pet already exists
-	local updateName = function(self,event)
+	local Update = function(self,event)
 		if self.Name then self.Name:UpdateTag(self.unit); end
+		if self.Level then self.Level:UpdateTag(self.unit); end
 	end
-	addon.pet:RegisterEvent("UNIT_PET",updateName);
+	addon.pet:RegisterEvent("UNIT_PET",Update);
+	--addon.pet:RegisterEvent("UNIT_CLASSIFICATION_CHANGED",Update);
 end
 	
 	
